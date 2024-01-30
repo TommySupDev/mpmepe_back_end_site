601b74cacb6422f5610e0a6d87564f6b0e4ccd7d
Miramar ALINGO <miramaralingo2013@gmail.com>
2024-01-30 14:49:49 +0200

    Ajout du support de l'authentification a deux facteurs grace au bundle SchebTwoFactorBundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
