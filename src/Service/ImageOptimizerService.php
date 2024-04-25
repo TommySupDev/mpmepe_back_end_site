15a274b3cb6bb471e5cbccb0c94942708622112f
Miramar ALINGO <miramaralingo2013@gmail.com>
2024-04-25 03:21:21 +0200

    Maj du fichier ImageOptimizerService

--- src/Service/ImageOptimizerService.php
+++ src/Service/ImageOptimizerService.php
@@ -19,15 +19,15 @@ class ImageOptimizerService
 
     public function resize(string $filename): void
     {
-        list($iwidth, $iheight) = getimagesize($filename);
-        $ratio = $iwidth / $iheight;
+//        list($iwidth, $iheight) = getimagesize($filename);
+//        $ratio = $iwidth / $iheight;
         $width = self::MAX_WIDTH;
         $height = self::MAX_HEIGHT;
-        if ($width / $height > $ratio) {
-            $width = $height * $ratio;
-        } else {
-            $height = $width / $ratio;
-        }
+//        if ($width / $height > $ratio) {
+//            $width = $height * $ratio;
+//        } else {
+//            $height = $width / $ratio;
+//        }
 
         $photo = $this->imagine->open($filename);
         $photo->resize(new Box($width, $height))->save($filename);
